32b582ff5e747baf17a2f3b4b6ea7a9b6d5ba4a4
picasso250 <cumt.xiaochi@gmail.com>
2013-11-13 14:42:01 +0800

    第一次

---
+++ doubanfm.html5.js
@@ -0,0 +1,59 @@
+(function(){
+    var makeRandomString = function (length) {
+        var text = "";
+        var possible = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        for( var i=0; i < length; i++ )
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        return text;
+    };
+
+    var doubanfmhtml5 = {
+        getRequestData: function() {
+            return {
+                type: 'n',
+                channel: -3,
+                from: 'mainsite',
+                r: makeRandomString(10)
+            };
+        },
+        changeUI: function () {
+            this.el = document.getElementById('fm-player');
+            this.wrap = $('div.player-wrap').html('加载中...');
+            this.playList();
+        },
+        playSound: function (songs, i) {
+            var that = this;
+            var song = songs[i];
+            console.log(song);
+            var html = '<img src="'+song.picture+'"><br>'+song.title+'<br>'+song.artist+'<br>';
+            if (song.like) {
+                html += '<red>♥';
+            };
+            this.wrap.html(html);
+            var soundfile = song.url;
+            this.el.mp3 = new Audio(soundfile);
+            this.el.mp3.play();
+            $(this.el.mp3).bind('ended', function () {
+                if (i < song.length) {
+                    that.playSound(songs, i+1);
+                } else {
+                    console.log('end of list');
+                    that.playList();
+                }
+            });
+        },
+        playList: function () {
+            console.log('playList');
+            var that = this;
+            $.getJSON('/j/mine/playlist', doubanfmhtml5.getRequestData(), function(ret) {
+                if (ret.r !== 0) {
+                    alert('get playlist fail');
+                    return;
+                }
+                that.playSound(ret.song, 0);
+            });
+        }
+    };
+
+    doubanfmhtml5.changeUI();
+})();
